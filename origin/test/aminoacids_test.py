a911f0dddce59fa4fb9f77e8ccde0200d292d90d
natalia-gouvea <nanaka.gouvea@gmail.com>
2014-11-05 19:52:04 -0200

    better find encoding, not working

--- origin/test/aminoacids_test.py
+++ origin/test/aminoacids_test.py
@@ -4,7 +4,19 @@ __author__ = 'natalia'
 
 class AminoacidsTest(TestCase):
 
-    def test_clump_edge(self):
+    def test_translate(self):
         lines = open("../data/translate.txt").read().splitlines()
         trans = aminoacids.translate_peptide(lines[0])
         self.assertEqual(trans, lines[1])
+
+    def test_find_peptide_encoding(self):
+        lines = open("../data/peptide_encoding.txt").read().splitlines()
+        strings = aminoacids.find_peptide_encoding(lines[0], lines[1])
+        self.assertEqual(set(strings), set(lines[2:]))
+
+    def test_better_find_peptide_encoding(self):
+        lines = open("../data/peptide_encoding.txt").read().splitlines()
+        strings = aminoacids.better_find_peptide_encoding(lines[0], lines[1])
+        self.assertEqual(len(lines[2:]), len(strings))
+        self.assertEqual(set(lines[2:]), set(strings))
+
