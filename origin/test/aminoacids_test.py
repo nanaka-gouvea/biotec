4b7f293888ce5f963f65147583a6b04b5ca369be
natalia-gouvea <nanaka.gouvea@gmail.com>
2014-11-06 11:27:53 -0200

    better peptide ok, brevis 0

--- origin/test/aminoacids_test.py
+++ origin/test/aminoacids_test.py
@@ -2,12 +2,13 @@ from unittest.case import TestCase
 from origin import aminoacids
 __author__ = 'natalia'
 
+
 class AminoacidsTest(TestCase):
 
     def test_translate(self):
         lines = open("../data/translate.txt").read().splitlines()
         trans = aminoacids.translate_peptide(lines[0])
-        self.assertEqual(trans, lines[1])
+        self.assertEqual(trans.replace(" ", ""), lines[1])
 
     def test_find_peptide_encoding(self):
         lines = open("../data/peptide_encoding.txt").read().splitlines()
@@ -16,7 +17,7 @@ class AminoacidsTest(TestCase):
 
     def test_better_find_peptide_encoding(self):
         lines = open("../data/peptide_encoding.txt").read().splitlines()
-        strings = aminoacids.better_find_peptide_encoding(lines[0], lines[1])
-        self.assertEqual(len(lines[2:]), len(strings))
-        self.assertEqual(set(lines[2:]), set(strings))
+        result = aminoacids.better_find_peptide_encoding(lines[0], lines[1])
+        self.assertEqual(len(lines[2:]), len(result))
+        self.assertEqual(set(lines[2:]), set(result))
 
