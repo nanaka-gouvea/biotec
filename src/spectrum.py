03e7c89412e639bb9446fa0f242857f90ec8d627
natalia-gouvea <nanaka.gouvea@gmail.com>
2014-11-13 10:23:10 -0200

    trim leader board spectrum

--- src/spectrum.py
+++ src/spectrum.py
@@ -93,49 +93,29 @@ def find_linear_peptides_by_spectrum(spec):
     return possible_peptides_from_spectrum(spec, "", False)
 
 
-def masses_possible_peptides_from_spectrum(spec, p, cyclic, amino_masses=None):
-    p_spec_size = (count_fragment_cyclic_peptide(len(p)) + 2) if cyclic else count_fragment_linear_peptide(len(p))
-    if p_spec_size == len(spec):
-        if theoretical_spectrum(p, cyclic) == spec:
-            if sum(p) == spec[-1]:
-                return p
-            else:
-                return []
-        else:
-            return []
-    branch = []
-    if amino_masses is None:
-        amino_masses = [str(x) for x in set(a_masses.values())]
-    b_aminoacids = list(amino_masses)
-    for k in amino_masses:
-        new = p.append(k)
-        if sum(new) in spec:
-            branch.extend(possible_peptides_from_spectrum(spec, new, cyclic, b_aminoacids))
-        else:
-            if len(new) == 1:
-                b_aminoacids.remove(k)
-    return branch
-
-
 def masses_find_cyclic_peptides_by_spectrum(spec):
-    possible = possible_peptides_from_spectrum(spec, "", True)
+    u_m_a = list(a_masses.keys())
+    #removes aminoacids which have homonimal masses
+    u_m_a.remove("L")
+    u_m_a.remove("Q")
+    possible = possible_peptides_from_spectrum(spec, "", True, u_m_a)
     result = set()
     for p in possible:
         result.add("-".join([str(s) for s in [a_masses[a] for a in p]]))
     return result
 
-
+#to linear cyclic should be false
 def peptide_score(peptide, spec, cyclic):
+    aux_spec = list(spec)
     score = 0
     pts = theoretical_spectrum(peptide, cyclic)
     lp = len(pts)
     i = 0
     while i < lp:
         sp = pts[i]
-        if sp in spec:
+        if sp in aux_spec:
             score += 1
-            #TODO melhorar performance
-            spec.remove(sp)
+            aux_spec.remove(sp)
             pts.remove(sp)
             lp -= 1
         else:
@@ -143,26 +123,91 @@ def peptide_score(peptide, spec, cyclic):
     return score
 
 
-def masses_possible_peptides_from_experimental_spectrum(spec, p, cyclic, amino_masses=None):
-    p_spec_size = (count_fragment_cyclic_peptide(len(p)) + 2) if cyclic else count_fragment_linear_peptide(len(p))
-    if p_spec_size == len(spec):
-        if peptide_score(p, spec, cyclic) >= 1:
-            if sum(p) == spec[-1]:
-                return p
+def expand_peptides_board(peps, spec):
+    expanded = {}
+    for p in peps:
+        for k in a_masses.keys():
+            expanded[p+k] = peptide_score(p, spec, False)
+    return expanded
+
+
+# Trim(Leaderboard, Spectrum, N, AminoAcid, AminoAcidMass)
+# for j = 1 to |Leaderboard
+#         Peptide = j-th peptide in Leaderboard
+# LinearScores(j) = LinearScore(Peptide, Spectrum)
+# sort Leaderboard according to the decreasing order of scores in LinearScores
+# sort LinearScores in decreasing order
+# for j = N + 1 to |Leaderboard|
+#   if LinearScores(j) < LinearScores(N)
+#     remove all peptides starting from the j-th peptide from Leaderboard
+# return Leaderboard
+
+
+def trim_peptides(leader_board, t):
+    llen = len(leader_board)
+    if llen > t:
+        sorted_scores = sorted(leader_board.values())
+        sorted_scores.reverse()
+        limit = sorted_scores[t]
+        for i in range(len(sorted_scores))[t:]:
+            low_s = sorted_scores[i]
+            if low_s < limit:
+                sorted_scores = sorted_scores[:t]
+                break
+        aux_leader_board = leader_board.copy()
+        for k,v in aux_leader_board.iteritems():
+            if v in sorted_scores:
+                continue
             else:
-                return []
-        else:
-            return []
-    branch = []
-    if amino_masses is None:
-        amino_masses = [str(x) for x in set(a_masses.values())]
-    b_aminoacids = list(amino_masses)
-    for k in amino_masses:
-        new = p.append(k)
-        if sum(new) in spec:
-            branch.extend(possible_peptides_from_spectrum(spec, new, cyclic, b_aminoacids))
-        else:
-            if len(new) == 1:
-                b_aminoacids.remove(k)
-    return branch
+                del leader_board[k]
+    return leader_board
+
+#     LEADERBOARDCYCLOPEPTIDESEQUENCING(Spectrum, N)
+#     Leaderboard = {empty peptide}
+#     LeaderPeptide = empty peptide
+#     while Leaderboard is non-empty
+#         Leaderboard = Expand(Leaderboard)
+#         for each Peptide in Leaderboard
+#             if Mass(Peptide) = ParentMass(Spectrum)
+#               if Score(Peptide, Spectrum) > Score(LeaderPeptide, Spectrum)
+#                 LeaderPeptide = Peptide
+#             else if Mass(Peptide) > ParentMass(Spectrum)
+#               remove Peptide from Leaderboard
+#         Leaderboard = Trim(Leaderboard, Spectrum, N)
+#     output LeaderPeptide
+
+def leader_board_experimental_spectrum(spec, cyclic, aminoacids, t):
+    "t is tolerance for the leaderboard"
+    parent_mass = spec[-1]
+    leader = ""
+    leader_board = {leader:0}
+    while len(leader_board) > 0:
+        # leader_board = expand_peptides(leader_board)
+        for pep in leader_board:
+            if total_mass(pep) == parent_mass:
+                #if
+                return leader
+    # if total_mass(p) == spec[-1]:
+    #     return [p]
+    # branch = [""]
+    # for b in branch:
+    #     n_branches = {}
+    #     for k in aminoacids:
+    #         new = b + k
+    #         n_branches[new] = linear_peptide_score(new, spec, cyclic)
+    #     branch.extend(trim_peptides(n_branches, t))
+    return leader
+
+
+def masses_find_cyclic_peptides_by_experimental_spectrum(spec):
+    u_m_a = list(a_masses.keys())
+    #removes aminoacids which have homonimal masses
+    u_m_a.remove("L")
+    u_m_a.remove("Q")
+    possible = leader_board_experimental_spectrum(spec, True, u_m_a, 7)
+    result = set()
+    for p in possible:
+        result.add("-".join([str(s) for s in [a_masses[a] for a in p]]))
+    return result
+
 
