dcafc4c1db8e3e8042e497c1cb4d8b9fbc31aae0
natalia-gouvea <nanaka.gouvea@gmail.com>
2014-11-18 18:29:16 -0200

    score motif

--- src/motif.py
+++ src/motif.py
@@ -2,8 +2,8 @@ __author__ = 'natalia'
 
 from origin import generate_neighbourhood
 from origin import pool
-from translation import get_file
 from origin import pattern_count_aprox
+from translation import get_file
 
 
 def motif_enumeration(dna, k, d):
@@ -21,3 +21,34 @@ def motif_enumeration(dna, k, d):
         if count == len(dna):
             patterns.add(n)
     return patterns
+
+
+lower_pool = "acgt"
+
+
+def score_motif(mots):
+    count = 0
+    for m in mots:
+        for l in m:
+            if l in lower_pool:
+                count += 1
+    return count
+
+def count_motif(mots):
+    prob = {"A": {}, "C":{}, "G":{}, "T":{}}
+    for m in mots:
+        for i in range(len(m)):
+            n = m[i].upper()
+            try:
+                prob[n][i] += 1
+            except KeyError:
+                prob[n][i] = 1
+    return prob
+
+motif_mx = [l.replace(" ", "") for l in get_file("/data/motif_matrix.txt").read().splitlines()]
+cmap = count_motif(motif_mx)
+
+for k, v in cmap.iteritems():
+    print k, ": ", ' '.join([str(x) for x in v.values()])
+
+
